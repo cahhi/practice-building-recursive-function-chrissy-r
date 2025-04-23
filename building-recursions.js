65c188068f76fc93b3f42704773f282bec318713
chrissy <chrissyrodgers20@gmail.com>
2025-04-22 19:35:03 -0500

    Fixed the code so it ran using recursion

--- building-recursions.js
+++ building-recursions.js
@@ -1,10 +1,26 @@
-function reverseString(userString) {
+/*function reverseString(userString) {
     if (userString.length === "" || userString === 1) {
         return userString;
         
+    } if(userString.length>1){
+        //return userString.split('').reverse().join('');
+        lastLetter = userString.slice(userString - 1, userString.length);
+        remainingLetter = userString.slice(0,userString.length -1);
+        return reverseString(lastLetter) + reverseString(remainingLetter);
     }
-        return userString.split('').reverse().join('');
-    
-    }
+}
 
-console.log(reverseString("c"));
+console.log(reverseString("hello"));*/
+
+
+function reverseString(str) {
+    if (str.length === 1 || str === "") {
+        return str
+    }
+    if (str.length > 1) {
+        lastLetter = str.slice(str.length - 1,str.length);
+        remainingLetters = str.slice(0,str.length - 1);
+        return reverseString(lastLetter) + reverseString(remainingLetters);
+    }
+}
+console.log(reverseString('Pizza'));
